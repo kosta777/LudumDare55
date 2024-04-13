fdc8f96f1e6b18ff45325461a0751d6747f1ad15
Galmir <galati.mirko@gmail.com>
2024-04-13 18:00:01 +0200

    added recipe logic
    TODO: fix comapring the ingredients

--- Recipes/recipe_base.gd
+++ Recipes/recipe_base.gd
@@ -2,10 +2,18 @@ class_name RecipeBase
 extends Resource
 
 @export var recipe_name : String
-
+@export var recipe_time_limit : int
 @export_category("Ingredients amount")
 @export_range(0,10,1) var black_souls : int
 @export_range(0,10,1) var red_souls : int
 @export_range(0,10,1) var purple_souls : int
-@export_range(0,10,1) var wings : int
 @export_range(0,10,1) var horns : int
+@export_range(0,10,1) var wings : int
+
+var ingredients_needed := {
+	Ingredients.Type.BLACK_SOUL : black_souls,
+	Ingredients.Type.RED_SOUL : red_souls,
+	Ingredients.Type.PURPLE_SOUL : purple_souls,
+	Ingredients.Type.HORNS : horns,
+	Ingredients.Type.WINGS : wings
+}
--- SummoningCircle.gd
+++ SummoningCircle.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var recipes : Array[RecipeBase]
+
 @onready var ingredient_amount_1: Label = $IngredientAmount1
 @onready var ingredient_amount_2: Label = $IngredientAmount2
 @onready var ingredient_amount_3: Label = $IngredientAmount3
@@ -8,17 +10,11 @@ extends Area2D
 @onready var sprite: TextureRect = $Sprite
 
 
-
 var ingredients_received := {}
 var can_summon := false
 
-
 func _ready() -> void:
-	pass
-	for ingredient in Ingredients.Type.keys():
-		ingredients_received[ingredient] = 0
-		
-	print(ingredients_received)
+	reset_ingredients_received()
 	update_labels()
 
 
@@ -26,7 +22,7 @@ func _on_body_entered(body: RawMaterialNode) -> void:
 	for child in body.carried_item.get_children():
 		if child is IngredientDrop:
 			print("ingredient is %s" % Ingredients.Type.keys()[child.ingredient_type])
-			ingredients_received[Ingredients.Type.keys()[child.ingredient_type]] += 1
+			ingredients_received[child.ingredient_type] += 1
 			body.queue_free()
 			print(ingredients_received)
 			update_labels()
@@ -41,3 +37,39 @@ func update_labels() -> void:
 
 	var tween: Tween = create_tween()
 	tween.tween_property(sprite, "modulate:v", 1, 0.25).from(15)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("summon"):
+		summon_demon()
+
+
+func summon_demon() -> void:
+	if not is_node_ready():
+		await ready
+		
+	if recipes.size() == 0:
+		return
+	
+	var can_summon := false
+	print("here are the %s recipes" % recipes.size())
+	print("here are your ingredients: %s" % ingredients_received)
+	for recipe in recipes:
+		print("%s: %s" % [recipe.recipe_name, recipe.ingredients_needed])
+		if recipe.ingredients_needed == ingredients_received:
+			can_summon = true
+			print("%s successful!" % recipe.recipe_name)
+			break
+	reset_ingredients_received()
+	
+	
+
+
+func reset_ingredients_received() -> void:
+	ingredients_received = {
+		Ingredients.Type.BLACK_SOUL : 0,
+		Ingredients.Type.RED_SOUL : 0,
+		Ingredients.Type.PURPLE_SOUL : 0,
+		Ingredients.Type.HORNS : 0,
+		Ingredients.Type.WINGS : 0
+	}
