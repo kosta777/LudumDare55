93251f4f4838a68ffee25ed9d00a7817dcc84050
Galmir <galati.mirko@gmail.com>
2024-04-13 15:13:42 +0200

    added summoning circle logic

--- Ingredients/ingredient_drop.gd
+++ Ingredients/ingredient_drop.gd
@@ -1,19 +1,24 @@
 class_name IngredientDrop
 extends Node2D
 
-@export var ingredient_type : Ingredients.Type
+@export var ingredient_type : Ingredients.Type : set = _set_ingredient
 
 @onready var icon: TextureRect = $icon
 @onready var name_label: Label = $name
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	if not is_node_ready():
 		await ready
-	icon.texture = Ingredients.INGREDIENT_ICON[ingredient_type]
-	name_label.text = Ingredients.INGREDIENT_NAME[ingredient_type]
+		
+	update_graphics(ingredient_type)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _set_ingredient(new_value : Ingredients.Type) -> void:
+	ingredient_type = new_value
+	update_graphics(ingredient_type)
+
+
+func update_graphics(new_value : Ingredients.Type):
+	icon.texture = Ingredients.INGREDIENT_ICON[new_value]
+	name_label.text = Ingredients.INGREDIENT_NAME[new_value]
