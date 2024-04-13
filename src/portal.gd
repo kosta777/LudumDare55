e6440a3baba050151cb9a57fe802f396f13c6d42
Timothy Personal <timothypersonal@timothys-macbook-pro.home>
2024-04-13 12:37:33 +0200

    add portal

--- src/portal.gd
+++ src/portal.gd
@@ -1,24 +1,42 @@
 extends Node2D
 
-
-var turn_on = false
+@export var spawn_time = 2 #seconds
 
 @onready var portal_text_label = $StateLabel
 @onready var portal_detection_area = $PlayerDetectionArea
+@onready var progress_bar = $ProgressBar
+@onready var timer = $Timer
 
+var turn_on = false
+var player_in_range = false
+var tween_loading = null
+var portal_loading = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	portal_text_label.text = str(turn_on)
-
-
-func _on_player_detection_area_area_entered(area):
-	print("player")
-
+	portal_text_label.text = str(portal_loading)
+	
+	if player_in_range and !portal_loading and Input.is_action_just_pressed("interact"):
+		portal_loading = true
+		timer.start(spawn_time)
+		progress_bar.value = 0
+		tween_loading = get_tree().create_tween()
+		tween_loading.tween_property(progress_bar, "value", 100, spawn_time)
+		
+	
 
 func _on_player_detection_area_body_entered(body):
-	print(body)
+	player_in_range = true
+
+
+func _on_player_detection_area_body_exited(body):
+	player_in_range = false
+
+
+func _on_timer_timeout():
+	print("timer is done")
+	portal_loading = false
