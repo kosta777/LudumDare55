3530c35861acd0ada2ae5a4b539b49bab007d08c
Galmir <galati.mirko@gmail.com>
2024-04-13 16:46:16 +0200

    added recipe resource

---
+++ Recipes/recipe_base.gd
@@ -0,0 +1,11 @@
+class_name RecipeBase
+extends Resource
+
+@export var recipe_name : String
+
+@export_category("Ingredients amount")
+@export_range(0,10,1) var black_souls : int
+@export_range(0,10,1) var red_souls : int
+@export_range(0,10,1) var purple_souls : int
+@export_range(0,10,1) var wings : int
+@export_range(0,10,1) var horns : int
