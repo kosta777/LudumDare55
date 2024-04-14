63933dc775baad385f006757e913f11d4905bb0b
Kosta Stojiljkovic <kostasto@proton.me>
2024-04-14 09:59:57 +0200

    wip

---
+++ src/task.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+@export var demon_icon: TextureRect
+@export var ingredients_container: Container
+
+@export var ingredients_window_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
