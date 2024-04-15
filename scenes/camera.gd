045f64a3cd5d946ec4e06e20c9f92ee0bc7091ec
Timothy Trovatelli <trovatelli.timothy@outlook.com>
2024-04-15 18:00:57 +0200

    add red screen when hit

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,9 +1,10 @@
 class_name CustomCamera extends Camera2D
 
 @export var movement_bounding_box: ColorRect
+@onready var color_rect = $ColorRect
+@onready var tween_rect = get_tree().create_tween()
 
 var shake_strength:float = 0
-
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +20,9 @@ func _process(delta):
 
 func apply_shake(strength = 6.0):
 	shake_strength = strength
+	tween_rect = get_tree().create_tween()
+	tween_rect.tween_property(color_rect, "color:a", .4, .1)
+	tween_rect.chain().tween_property(color_rect, "color:a", 0, .3)
 
 func random_offset() -> Vector2:
 	return Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength, shake_strength))
